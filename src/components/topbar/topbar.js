15e3de42f8c2b753be513a4c38d7982e5d551758
Swatboy76 <lukenodarshvili@gmail.com>
2023-12-19 18:53:19 +0000

    Added most of HTML, working on SCSS and co-working on partials to link ASAP

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,16 +1,31 @@
 import "./topbar.scss";
+import logo from "../../assets/logo/BrainFlix-logo.svg";
+import userPfP from "../../assets/images/Mohan-muruge.jpg";
+import uploadarrow from "../../assets/icons/upload.svg";
 
 function topbar() {
   return (
-    <header className="App">
-      <img
-        className="headerbar__logo--phonescreen"
-        src="luke-nodarshvili-brainflix\src\assets\logo\BrainFlix-logo.svg"
-        alt="Logo"
-      />
-      <form>
-        <input type="text" id="searchBar" placeholder="Search"></input>
-      </form>
+    <header className="headerbar">
+      <img className="headerbar__logo" src={logo} alt="Logo" />
+      <div className="headerbar__navimg">
+        <form>
+          <input
+            className="headerbar__navimg headerbar__form"
+            type="text"
+            id="searchBar"
+            placeholder="Search"
+          ></input>
+        </form>
+        <img src={userPfP} className="headerbar__navimg--phonescreen" />
+      </div>
+      <div className="headerbar__upload">
+        <img
+          className="headerbar__upload headerbar__uploadimage"
+          src={uploadarrow}
+          alt="Logo"
+        />
+        <p>UPLOAD</p>
+      </div>
     </header>
   );
 }
