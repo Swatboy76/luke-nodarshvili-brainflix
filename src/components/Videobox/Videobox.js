5c45f4d7bd3018ea25ad70b832f72780be6acb90
Swatboy76 <lukenodarshvili@gmail.com>
2023-12-23 00:20:54 +0000

    Completed videobox for current stage

--- src/components/Videobox/Videobox.js
+++ src/components/Videobox/Videobox.js
@@ -1,7 +1,18 @@
 import "./Videobox.scss";
 
 function Videobox() {
-  return <video className="videobox"></video>;
+  return (
+    <main className="videobox">
+      <video
+        id="videoplayer"
+        controls
+        width="250"
+        poster="https://i.imgur.com/l2Xfgpl.jpg"
+      >
+        <source src="https://project-2-api.herokuapp.com/stream" />
+      </video>
+    </main>
+  );
 } //get this done for fucks sake
 
 export default Videobox;
