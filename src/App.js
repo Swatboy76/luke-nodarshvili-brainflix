bc6ad5f79c6b4a4be521e3505dcab78c5c69a8ec
Swatboy76 <lukenodarshvili@gmail.com>
2023-12-23 00:20:48 +0000

    Completed the video box minus interactivity (can not be completed at current time)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Topbar from "./components/topbar/topbar.js";
+import Videobox from "./components/Videobox/Videobox.js";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <Videobox />
     </div>
   );
 }
---
+++ src/components/Videobox/Videobox.js
@@ -0,0 +1,7 @@
+import "./Videobox.scss";
+
+function Videobox() {
+  return <video className="videobox"></video>;
+} //get this done for fucks sake
+
+export default Videobox;
