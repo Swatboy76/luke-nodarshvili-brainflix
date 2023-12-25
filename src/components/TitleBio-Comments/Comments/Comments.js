c3f70bdaf57ec88abb8b9555ca75c2e0bf076fb2
Swatboy76 <lukenodarshvili@gmail.com>
2023-12-25 22:55:22 +0000

    Completed version 1 of comments. will return to

--- src/components/TitleBio-Comments/Comments/Comments.js
+++ src/components/TitleBio-Comments/Comments/Comments.js
@@ -1,7 +1,32 @@
 import "./Comments.scss";
-import React, { useState } from "react";
+import NewComment from "./NewComment/NewComment.js";
+//<MakeComment />
 
-function Comments() {
-  return <></>;
+function Comments({ currentVideoID, data }) {
+  let matchingData = data.find((item) => item.id === currentVideoID);
+  let commentsData = matchingData?.comments;
+  return (
+    <section className="CommentsArea">
+      <NewComment />
+      <ul className="CommentsArea__list">
+        {commentsData.map((comment) => {
+          return (
+            <li key={comment.id} className="IndividualComment">
+              <img className="IndividualComment__Picture" />
+              <div className="IndividualComment__Contents">
+                <div className="IndividualComment__NameDate">
+                  <p>{comment.name}</p>
+                  <p className="IndividualComment__NameDate--Date">
+                    {comment.timestamp}
+                  </p>
+                </div>
+                <p>{comment.comment}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
 export default Comments;
---
+++ src/components/TitleBio-Comments/Comments/NewComment/NewComment.js
@@ -0,0 +1,34 @@
+import "./NewComment.scss";
+import pfp from "../../../../assets/images/Mohan-muruge.jpg";
+function NewComment() {
+  const numbrOfComments = "N/A";
+  return (
+    <section className="NewComment">
+      <p>{numbrOfComments} Comments</p>
+      <div className="imageform">
+        <img className="imageform__image" src={pfp} alt="PFP" />
+        <form className="form">
+          <div className="form__left">
+            <label className="form__left--label" for="commentbox">
+              JOIN THE CONVERSATION
+            </label>
+            <input
+              className="form__left--input"
+              type="textbox"
+              name="commentbox"
+              id="commentbox"
+            />
+          </div>
+          <input
+            type="submit"
+            name="submit"
+            id="submit"
+            className="form__right"
+            placeholder="Comment"
+          />
+        </form>
+      </div>
+    </section>
+  );
+}
+export default NewComment;
